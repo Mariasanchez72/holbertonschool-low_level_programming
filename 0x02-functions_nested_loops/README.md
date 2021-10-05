2eff974ee2ac357385b02e5aa43fbeb6334164bf
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-05 13:29:49 -0700

    first commit

--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-leme
+leeme
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('m');
+	_putchar('a');
+	_putchar('i');
+	_putchar('n');
+	_putchar('\n');
+
+return (0);
+}
