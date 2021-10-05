db9a698363701a1d11689391611371dc871404a0
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-05 13:46:48 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,24 @@
 #include "main.h"
+
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: Always 0 (Success)
+ * Description: Prints _putchar with _putchar
+ *
+ * Return: always 0
  */
+
 int main(void)
 {
-	char *sh = "main.c";
+	int i = 0;
+	char c;
+	char s[] = "_putchar\n";
 
-	while (*sh)
+	while (i <= 9)
 	{
-		_putchar(*sh);
-		sh++;
+		c = s[i];
+		_putchar(c);
+		i++;
 	}
-	_putchar('\n');
-
 	return (0);
 }
