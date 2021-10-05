4141b7822445826e619d8269c8ebbf135f24e57f
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-05 14:42:40 -0700

    first commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,22 @@
 #include "main.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
  */
-int main(void)
-{
-	char *sh = "main";
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 
-	return (0);
-}
-
+#endif /*_HOLBERTON_H_ */
