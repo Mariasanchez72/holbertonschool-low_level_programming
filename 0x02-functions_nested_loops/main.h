bb15c9c85ce92111d65b3e02128ee6c5bdc8a1da
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-05 14:52:42 -0700

    first commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,5 @@
 #include "main.h"
 
-/*
- * Structs, enums and unions definitions
- * Typedefs
- * Function prototypes
- */
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -18,4 +12,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
+int main(void);
