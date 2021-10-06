f8ead62d2eaf528cb9125d884eb1016e3a1c94a0
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-06 08:09:12 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-char c[] = "_putchar\n";
+char c[] = "_putchar";
 int i;
 
-for (i = 0; i < 9; i++)
+for (i = 0; i < 8; i++)
 {
 _putchar(c[i]);
 }
-_putchar('\n');
+
 return (0);
 }
