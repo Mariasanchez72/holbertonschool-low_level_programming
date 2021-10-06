bfe129edb1075dafe5a8068f6da1a90105ba3131
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-05 17:54:00 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
  *Return: always 0
  */
 
-int main(void)/
+int main(void)
 {
 	int i = 0;
 	char c;
-	char s[] = "_putchar/n";
+	char s[] = "_putchar";
 
-	while (i <= 9)
+	while (i <= 8)
 	{
 	       c = s[i];
 	       _putchar(c);
