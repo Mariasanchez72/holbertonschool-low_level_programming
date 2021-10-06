c5c92a2e147a0d6fc50b2459c47633afb6c347db
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-05 18:56:53 -0700

    first commit

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -6,10 +6,13 @@
  */
 int main(void)
 {
-	_putchar('m');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
 	_putchar('a');
-	_putchar('i');
-	_putchar('n');
+	_putchar('r');
 	_putchar('\n');
 
 return (0);
