ca5e20193a0438cac64001e62b6088468c8db8e6
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-08 11:42:17 -0700

    first commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,14 @@
  */
 void print_most_numbers(void)
 {
-  int a;
-  
-  for (a = 48; a >= 48 && a <= 57; a++)
-    {
-      if (a != 50 && a != 52)
+	int a;
+
+	for (a = 48; a >= 48 && a <= 57; a++)
 	{
-	  _putchar(a);
+		if (a != 50 && a != 52)
+		{
+			_putchar(a);
+		}
 	}
-    }
-  _putchar(10);
+	_putchar(10);
 }
