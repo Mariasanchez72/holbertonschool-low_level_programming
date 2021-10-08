92381fd371139469473c06f720c03d6a521a1213
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-07 18:20:50 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - Prints a straight line in the terminal.
+ * Return: Void.
+ * @n: Number of times the line will be printed.
+ */
+void print_line(int n)
+{
+  int lines;
+
+  if (n <= 0)
+    {
+      _putchar(10);
+    }
+  else
+    {
+      for (lines = 0; lines < n; lines++)
+	{
+	  _putchar('_');
+	}
+      _putchar(10);
+    }
+}
