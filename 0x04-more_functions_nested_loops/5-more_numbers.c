b97eec918717eca33e8861f1eda20f6a154d2f50
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-07 18:10:10 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - Prints numbers from 0 to 14 ten times
+ * Return: Void.
+ */
+void more_numbers(void)
+{
+  int a;
+  int b;
+
+  for (a = 0; a < 10; a++)
+    {
+      for (b = 0; b <= 14; b++)
+	{
+	  if ((b / 10) != 0)
+	    {
+	      _putchar((b / 10) + '0');
+	    }
+	  _putchar((b % 10) + '0');
+	}
+      b = 0;
+      _putchar(10);
+    }
+}
+
