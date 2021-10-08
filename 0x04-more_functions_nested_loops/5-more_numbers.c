58acc9228e731110a7a272c4ccf9d0a3660c0060
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-08 11:45:48 -0700

    first commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,21 +5,20 @@
  */
 void more_numbers(void)
 {
-  int a;
-  int b;
+	int a;
+	int b;
 
-  for (a = 0; a < 10; a++)
-    {
-      for (b = 0; b <= 14; b++)
+	for (a = 0; a < 10; a++)
 	{
-	  if ((b / 10) != 0)
-	    {
-	      _putchar((b / 10) + '0');
-	    }
-	  _putchar((b % 10) + '0');
+		for (b = 0; b <= 14; b++)
+		{
+			if ((b / 10) != 0)
+			{
+				_putchar((b / 10) + '0');
+			}
+			_putchar((b % 10) + '0');
+		}
+		b = 0;
+		_putchar(10);
 	}
-      b = 0;
-      _putchar(10);
-    }
 }
-
