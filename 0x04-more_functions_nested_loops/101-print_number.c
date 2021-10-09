2b86281b238fcd94e7cd68e76505abbe95fd0097
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-08 19:14:35 -0700

    first commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_number - print number putchar
  * @n:integer
@@ -15,8 +14,8 @@ void print_number(int n)
 		_putchar('-');
 		x = -x;
 	}
-	if ((x / 10 > 0)
-	    print_number(x / 10);
+	if (x / 10 > 0)
+		print_number(x / 10);
 
-	    _putchar(x % 10 + '0');
+	_putchar(x % 10 + '0');
 }
