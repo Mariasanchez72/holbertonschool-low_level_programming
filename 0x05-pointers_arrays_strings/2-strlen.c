8e36234ab781045fd440bc60579efb5f74edbb97
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-11 15:24:19 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strlen - Returns lenght of a string
+ *@s: char
+ *Return: lenght of string in s
+ */
+
+int_strlen(char *s)
+{
+	int x;
+	x = 0;
+	while (s[] != '\0')
+	{
+		x++;
+	}
+	return(x);
+}
