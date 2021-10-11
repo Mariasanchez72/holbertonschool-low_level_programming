9e909e889030c1a4ad99f65f050d20609c9efa62
Mariasanchez72 <mariadelpilarsl@hotmail.com>
2021-10-11 16:39:46 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* puts2 - Prints every other char of a string.
+* @str: a string
+* Return: odds
+*/
+
+void puts2(char *str)
+{
+int x;
+int p = 0;
+int m = 0;
+char *y;
+x = 0;
+y = str;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	if (x % 2 != 0)
+	{
+	m = 1;
+	}
+	while (p < ((x / 2) + m))
+	{
+	_putchar(*y);
+	p++;
+	y += 2;
+	}
+	_putchar('\n');
+}
